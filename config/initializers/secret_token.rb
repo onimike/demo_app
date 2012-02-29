2f744e919315a185fad5cfa2666c7399ffac01fd
onimike <onimike@csu.fullerton.edu>
2012-02-28 22:01:34 -0800

    Putting this back after deleting it like an idiot

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'f898821bb272a9a198b58ffcafc120bed27266f8c76e9eac4ca94a730e1c87deaff6abfff85c3dd01f1ea8f7813cdbc7b87efe31ffea03bb157d4e38b4e7dbd5'
